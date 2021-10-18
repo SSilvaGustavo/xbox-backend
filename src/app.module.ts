f23d71923f1a8dc0f2b9f539b6676e0e4c3f963f
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-10-18 19:57:35 -0300

    feat(auth): init backend authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { UserModule } from './user/user.module';
 import { ProfileModule } from './profile/profile.module';
 import { GenreModule } from './genre/genre.module';
 import { GamesModule } from './games/games.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, ProfileModule, GamesModule, GenreModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [UserModule, ProfileModule, GamesModule, GenreModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, PrismaService, AuthService],
 })
 export class AppModule {}
